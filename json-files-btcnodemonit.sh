863dbb562cf8e880f63813496251ed19724fdd32
m28ray <307800295+m28ray@users.noreply.github.com>
2026-08-17 23:50:31 -0500

    Add files via upload
    This script reads data from .json files rather than querying a bitcoin node. Import the .json files to a directory and the script will create the output similar to the project's screenshot.
    Files:  
    getblockcount.json
    getnetworkinfo.json
    getnettotals.json
    getmempoolinfo.json
    getblockstats.json
    getpeerinfo.json

---
+++ json-files-btcnodemonit.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/env bash
+
+# MIT License
+# 
+# Copyright (c) 2026 m28ray
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+################################################
+#
+# How to use:
+#
+# json-files-btcnodemonit.sh
+#   Get data from .json files in the directory specified in the
+#  "jsonDir" variable under the "User defined variable" section
+#   then exit
+#
+# json-files-btcnodemonit.sh [jsonDir]
+#   Get data from .json files in the directory added after the 
+#   script name then exit
+#
+#   Example: 
+#   ./json-files-btcnodemonit.sh "/path/to/myData"
+#
+################################################
+#
+# User defined variable
+#
+# jsonDir
+# The directory with the .json files. Bitcoin Knots RPC returns
+# .json formatted results. The result can be saved to a .json file.
+jsonDir=""
+#
+#
+################################################
+
+set -o errexit #Exit immediately if any command returns a non-zero exit status
+# set -o xtrace #Uncomment for debugging
+
+files=(
+getblockcount.json
+getnetworkinfo.json
+getnettotals.json
+getmempoolinfo.json
+getblockstats.json
+getpeerinfo.json
+)
+
+showBlockStats=1
+thousandsSeparator=0
+if awk -W version 2>/dev/null |grep --quiet "mawk"; then
+  thousandsSeparator=1
+fi
+
+# Function bytesPrefix
+# Number of arguments: 2
+# Arguments: Bitcoin-cli output, The item to match
+function bytesPrefix {
+  local line text bytes prefix negSign
+  line=$(grep "\"$2\"" <<< "$1")
+  text="$2"
+  bytes=$(grep -E --only-matching "[0-9]+" <<< "$line")
+  if grep --quiet -- "-[0-9]" <<< "$line"; then
+    negSign="-"
+  else
+    negSign=""
+  fi
+
+  prefix=""
+  if [[ $bytes -ge 1000000000 ]]; then
+    prefix="G"
+    bytes=$(awk -v byte="$bytes" 'BEGIN {print byte/1000000000}')
+  elif [[ $bytes -ge 1000000 ]]; then
+    prefix="M"
+    bytes=$(awk -v byte="$bytes" 'BEGIN {print byte/1000000}')
+  elif [[ $bytes -ge 1000 ]]; then
+    prefix="K"
+    bytes=$(awk -v byte="$bytes" 'BEGIN {print byte/1000}')
+  fi
+
+  if [[ $thousandsSeparator -eq 1 ]]; then
+    printf "  %s: %s%'.2f %sB\n" "$text" "$negSign" "$bytes" "$prefix"
+  else
+    printf "  %s: %s%.2f %sB\n" "$text" "$negSign" "$bytes" "$prefix"
+  fi
+}
+
+
+# Function minutesSinceMined
+# Number of arguments: 1
+# Argument: Block stats
+function minutesSinceMined {
+  local blockTime unixSeconds
+  blockTime=$(grep "\"time\"" <<<"$1" |grep -E --only-matching "[0-9]+")
+  unixSeconds=$(date +%s)
+  awk -v unixSeconds="$unixSeconds" -v blockTime="$blockTime" \
+    'BEGIN {printf "%.1f min ago\n", (unixSeconds-blockTime)/60}'
+}
+
+
+# Function validateJsonDir
+# Number of arguments: 0
+function validateJsonDir {
+  if [[ -z "$jsonDir" ]]; then
+    local helpText="Specify a directory on the command line or in the script variable.\n\nHow to use:\n\njson-files-btcnodemonit.sh\n  Get data from .json files in the directory specified in the \"jsonDir\" variable\n  under the \"User defined variable\" section of this script.\n\ndata-from-json-files-btcnodemonit.sh [jsonDir]\n./json-files-btcnodemonit.sh \"/path/to/myData\"\n  Get data from .json files in the directory added after the script name.\n\n"
+    printf '%b' "$helpText"
+    exit 1
+  fi
+  if ! [[ "$jsonDir" =~ ^[\ a-zA-Z0-9/_.-]+$ ]]; then
+    echo "Error: \"jsonDir\" has invalid characters"
+    exit 1
+  fi
+  if ! [[ -d "$jsonDir"  ]]; then
+    echo "Error: $jsonDir is not a directory"
+    exit 1
+  fi
+  if ! [[ -r "$jsonDir"  ]]; then
+    echo "Error: $jsonDir is not readable by you"
+    exit 1
+  fi
+  for f in ${files[@]}; do
+    if ! [[ -r "$jsonDir/$f" ]]; then
+      echo "Error: $f is not readable by you"
+      exit 1
+    fi
+  done
+}
+
+
+if [[ -n "$1" ]]; then
+  jsonDir="$1"
+fi
+validateJsonDir
+
+# Get data
+gbc=$(< "$jsonDir/${files[0]}") # getblockcount
+gni=$(< "$jsonDir/${files[1]}") # getnetworkinfo
+gnt=$(< "$jsonDir/${files[2]}") # getnettotals
+gmi=$(< "$jsonDir/${files[3]}") # getmempoolinfo
+gbs=$(< "$jsonDir/${files[4]}") # getblockstats
+bip110Count=$(grep --count "REDUCED_DATA?" "$jsonDir/${files[5]}" \
+            || true) # getpeerinfo
+
+# More data
+timeMilliS=$(grep '"timemillis"' <<<"$gnt" \
+           | grep -E --only-matching "[0-9]+" \
+           | awk '{printf "%.0f", $1/1000 }')
+date=$(date -d @$timeMilliS "+%Y-%m-%d %I:%M:%S %p %Z")
+
+
+# Format output
+echo "  date: $date"
+echo "$gni" |grep "connections" |tr -d '",' |sed 's/    //'
+echo "  bip110 peers: $bip110Count"
+bytesPrefix "$gnt" "totalbytesrecv"
+bytesPrefix "$gnt" "totalbytessent"
+bytesPrefix "$gnt" "bytes_left_in_cycle"
+echo "$gnt" |grep "time_left_in_cycle" |grep -E --only-matching "[0-9]+" \
+     |awk '{printf "  time_left_in_cycle: %.2f h\n", $1/60/60}'
+echo ""
+echo "$gmi" | grep 'size' |tr -d '",' | sed 's/size/mempool tx count/'
+bytesPrefix "$gmi" "bytes" | sed 's/B$/vB/'| sed 's/bytes/mempool virtual bytes/'
+echo "$gmi" | grep 'total_fee' |tr -d '",' | sed 's/$/ BTC/' \
+     | sed 's/total_fee/mempool total_fee/'
+bytesPrefix "$gmi" "usage" |sed 's/usage/mempool memory usage/'
+echo ""
+
+printf "  block: $gbc"
+if [[ showBlockStats -eq 0 ]]; then
+  echo ""
+  exit 0
+fi
+echo "  $(minutesSinceMined "$gbs")"
+bytesPrefix "$gbs" "total_size"
+bytesPrefix "$gbs" "total_weight" | sed 's/B$/WU/'
+echo "$gbs" | grep 'utxo_increase' | tr -d '",'
+bytesPrefix "$gbs" "utxo_size_inc"
+bytesPrefix "$gbs" "utxo_size_inc_actual"
+echo "$gbs" | grep '"txs"' |tr -d '",'
+totalOut=$(echo "$gbs" | grep '"total_out"' |grep -E --only-matching "[0-9]+")
+if [[ $thousandsSeparator -eq 1 ]]; then
+  awk -v tOut="$totalOut" 'BEGIN {printf "  total_out: %\047.2f BTC\n", tOut/100000000}'
+else
+  awk -v tOut="$totalOut" 'BEGIN {printf "  total_out: %.2f BTC\n", tOut/100000000}'
+fi
+echo "$gbs" | grep -E '"avgfeerate"|"minfeerate"|"maxfeerate"' \
+     |tr -d '",' | sed 's|$| sat/vB|'
+echo "$gbs" | grep --after-context=5 '"feerate_percentiles"' \
+     |tr -d '\n"[' |sed 's/: /:/' |sed 's/    / /g'
+echo ""
