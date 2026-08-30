be802c136257da2c6a91d9419aa38b3e088276ca
m28ray <307800295+m28ray@users.noreply.github.com>
2026-08-29 22:06:07 -0500

    Add files via upload
    Corrected a formatting problem in bytesPrefix that caused the script to fail in Redhat-based distros when the locale used period as thousands separator and comma as decimal.
    
    Added printInteger function to extract and print integer numbers.
    
    The bytesPrefix function now takes 4 arguments
    Arguments: Bitcoin-cli output, The item to match, The display name, The unit 
    
    Show time since last block was mined in hours after 180 minutes
    
    Locale improvements

--- btcnodemonit.sh
+++ btcnodemonit.sh
@@ -51,60 +51,83 @@ bitcoinCli="bitcoin-cli"
 #
 ################################################
 
-set -o errexit #Exit immediately if any command returns a non-zero exit status
-# set -o xtrace #Uncomment for debugging
+set -o errexit # Exit immediately if any command returns a non-zero exit status
+# set -o xtrace # Uncomment for debugging
 
 # bitcoinDir default is "/" if empty
 bitcoinDir="${bitcoinDir:-/}"
 
-thousandsSeparator=0
-if awk -W version 2>/dev/null |grep --quiet "mawk"; then
-  thousandsSeparator=1
-fi
-
 # Function bytesPrefix
-# Number of arguments: 2
-# Arguments: Bitcoin-cli output, The item to match
+# Number of arguments: 4
+# Arguments: Bitcoin-cli output, The item to match, The display name, The unit
 function bytesPrefix {
-  local line text bytes prefix negSign
-  line=$(echo "$1" | grep "\"$2\"")
-  text="$2"
-  bytes=$(echo "$line" | grep -E --only-matching "[0-9]+")
-  if echo "$line" | grep --quiet -- "-[0-9]"; then
+  local line text bytes prefix negSign unit
+  line=$(grep "\"$2\"" <<< "$1")
+  text="$3"
+  bytes=$(grep -E --only-matching "[0-9]+" <<< "$line")
+  if grep --quiet -- "-[0-9]" <<< "$line"; then
     negSign="-"
   else
     negSign=""
+  fi
+  unit="B"
+  if ! [[ -z $4 ]]; then
+    unit="$4"
   fi
 
   prefix=""
   if [[ $bytes -ge 1000000000 ]]; then
     prefix="G"
-    bytes=$(awk -v byte="$bytes" 'BEGIN {print byte/1000000000}')
+    bytes=$(awk -v byte="$bytes" -v q="'" 'BEGIN {printf "%"q".2f", byte/1000000000}')
   elif [[ $bytes -ge 1000000 ]]; then
     prefix="M"
-    bytes=$(awk -v byte="$bytes" 'BEGIN {print byte/1000000}')
+    bytes=$(awk -v byte="$bytes" -v q="'" 'BEGIN {printf "%"q".2f", byte/1000000}')
   elif [[ $bytes -ge 1000 ]]; then
     prefix="K"
-    bytes=$(awk -v byte="$bytes" 'BEGIN {print byte/1000}')
+    bytes=$(awk -v byte="$bytes" -v q="'" 'BEGIN {printf "%"q".2f", byte/1000}')
   fi
 
-  if [[ $thousandsSeparator -eq 1 ]]; then
-    printf "  %s: %s%'.2f %sB\n" "$text" "$negSign" "$bytes" "$prefix"
-  else
-    printf "  %s: %s%.2f %sB\n" "$text" "$negSign" "$bytes" "$prefix"
-  fi
+  printf "  %s: %s%s %s%s\n" "$text" "$negSign" "$bytes" "$prefix" "$unit"
 }
-
 
 # Function minutesSinceMined
 # Number of arguments: 1
 # Argument: Block stats
 function minutesSinceMined {
   local blockTime unixSeconds
-  blockTime=$(echo "$1" |grep "\"time\"" |grep -E --only-matching "[0-9]+")
   unixSeconds=$(date +%s)
-  awk -v unixSeconds="$unixSeconds" -v blockTime="$blockTime" \
-    'BEGIN {printf "%.1f min ago\n", (unixSeconds-blockTime)/60}'
+  blockTime=$(grep "\"time\"" <<<"$1" |grep -E --only-matching "[0-9]+")
+  if [[ $(( ($unixSeconds-$blockTime)/60 )) -lt 180 ]]; then
+    awk -v unixSeconds="$unixSeconds" -v blockTime="$blockTime" \
+    'BEGIN {printf "%.1f min", (unixSeconds-blockTime)/60}'
+  else
+    awk -v unixSeconds="$unixSeconds" -v blockTime="$blockTime" \
+    'BEGIN {printf "%.1f h", (unixSeconds-blockTime)/60/60}'
+  fi
+}
+
+# Function printInteger
+# Number of arguments: 4
+# Arguments: Bitcoin-cli output, The item to match, The display name, The unit
+# Example: printInteger "$gmi" "size" "mempool tx count" ""
+function printInteger {
+  local number
+  number="$(grep "\"$2\"" <<< "$1" | grep -E --only-matching -- "-?[0-9]+" \
+          || echo "No matching integer")"
+  printf "  %s: %'.0f %s\n" "$3" "$number" "$4"
+}
+
+function detectCommaAsDecimalSeparator {
+  # Formats 0.5 and checks if the output contains a comma
+  # To make printf reliable for locale detection, you must check the output
+  # string rather than the exit status - Google
+  local output
+  output=$(printf "%f" 0.5 2>/dev/null)
+  if [[ "$output" =~ , ]]; then
+    echo "true"
+  else
+    echo "false"
+  fi
 }
 
 
@@ -130,10 +153,9 @@ bip110Count=$($bitcoinCli getpeerinfo |grep --count "REDUCED_DATA?" || true)
 if df --output=avail --block-size=1 / >/dev/null 2>&1; then
   diskAvail=$(df --output=avail --block-size=1 "$bitcoinDir" |grep --invert-match "Avail")
 else
-  # 'df' command options for MAC OS. Suggested by AI. Untested.
+  # 'df' command options for MAC OS. Suggested by AI.
   # Multiply 4th colum by 1024 to convert KB into bytes
-  diskAvail=$(df -k "$bitcoinDir" | tail -1 | awk '{printf "%.0f", $4 * 1024}')
-  #diskAvail=0
+  diskAvail=$(df -k "$bitcoinDir" | tail -n 1 | awk '{printf "%.0f", $4 * 1024}')
 fi
 systemLoad=$(uptime |grep -E --only-matching "load average.+")
 hostName=$(hostname)
@@ -142,40 +164,44 @@ date=$(date "+%Y-%m-%d %I:%M:%S %p %Z")
 
 # Format output
 echo "  $hostName  $date"
-echo "$gni" |grep "connections" |tr -d '",' |sed 's/    //'
+printInteger "$gni" "connections" "connections" ""
+printInteger "$gni" "connections_in" "connections_in" ""
+printInteger "$gni" "connections_out" "connections_out" ""
 echo "  bip110 peers: $bip110Count"
-bytesPrefix "$gnt" "totalbytesrecv"
-bytesPrefix "$gnt" "totalbytessent"
-bytesPrefix "$gnt" "bytes_left_in_cycle"
-echo "$gnt" |grep "time_left_in_cycle" |grep -E --only-matching "[0-9]+" \
+bytesPrefix "$gnt" "totalbytesrecv" "totalbytesrecv" ""
+bytesPrefix "$gnt" "totalbytessent" "totalbytessent" ""
+bytesPrefix "$gnt" "bytes_left_in_cycle" "bytes_left_in_cycle" ""
+grep '"time_left_in_cycle"' <<< "$gnt" |grep -E --only-matching "[0-9]+" \
      |awk '{printf "  time_left_in_cycle: %.2f h\n", $1/60/60}'
 echo ""
-echo "$gmi" | grep 'size' |tr -d '",' | sed 's/size/mempool tx count/'
-bytesPrefix "$gmi" "bytes" | sed 's/B$/vB/'| sed 's/bytes/mempool virtual bytes/'
-echo "$gmi" | grep 'total_fee' |tr -d '",' | sed 's/$/ BTC/' \
-     | sed 's/total_fee/mempool total_fee/'
-bytesPrefix "$gmi" "usage" |sed 's/usage/mempool memory usage/'
+printInteger "$gmi" "size" "mempool tx count" ""
+bytesPrefix "$gmi" "bytes" "mempool virtual bytes" "vB"
+total_fee=$(grep '"total_fee"' <<< "$gmi" |grep -E --only-matching "[0-9.]+")
+if [[ "$(detectCommaAsDecimalSeparator)" == "true" ]]; then
+  total_fee="$(tr '.' ',' <<< "$total_fee")"
+fi
+echo "  mempool total_fee: $total_fee BTC"
+bytesPrefix "$gmi" "usage" "mempool memory usage" ""
 
 echo ""
-echo "  block: $gbc  $minutesSM"
-bytesPrefix "$gbs" "total_size"
-bytesPrefix "$gbs" "total_weight" | sed 's/B$/WU/'
-echo "$gbs" | grep 'utxo_increase' | tr -d '",'
-bytesPrefix "$gbs" "utxo_size_inc"
-bytesPrefix "$gbs" "utxo_size_inc_actual"
-echo "$gbs" | grep '"txs"' |tr -d '",'
-totalOut=$(echo "$gbs" | grep '"total_out"' |grep -E --only-matching "[0-9]+")
-if [[ $thousandsSeparator -eq 1 ]]; then
-  awk -v tOut="$totalOut" 'BEGIN {printf "  total_out: %\047.2f BTC\n", tOut/100000000}'
-else
-  awk -v tOut="$totalOut" 'BEGIN {printf "  total_out: %.2f BTC\n", tOut/100000000}'
-fi
-echo "$gbs" | grep -E '"avgfeerate"|"minfeerate"|"maxfeerate"' \
-     |tr -d '",' | sed 's|$| sat/vB|'
-echo "$gbs" | grep --after-context=5 '"feerate_percentiles"' \
+echo "  block: $gbc  $minutesSM ago"
+bytesPrefix "$gbs" "total_size" "total_size" ""
+bytesPrefix "$gbs" "total_weight" "total_weight" "WU"
+printInteger "$gbs" "utxo_increase" "utxo_increase" ""
+printInteger "$gbs" "utxo_increase_actual" "utxo_increase_actual" ""
+bytesPrefix "$gbs" "utxo_size_inc" "utxo_size_inc" ""
+bytesPrefix "$gbs" "utxo_size_inc_actual" "utxo_size_inc_actual" ""
+printInteger "$gbs" "txs" "txs" ""
+totalOut=$(grep '"total_out"' <<< "$gbs" |grep -E --only-matching "[0-9]+")
+awk -v tOut="$totalOut" -v q="'" \
+    'BEGIN {printf "  total_out: %"q".2f BTC\n", tOut/100000000}'
+printInteger "$gbs" "avgfeerate" "avgfeerate" "sat/vB"
+printInteger "$gbs" "minfeerate" "minfeerate" "sat/vB"
+printInteger "$gbs" "maxfeerate" "maxfeerate" "sat/vB"
+grep --after-context=5 '"feerate_percentiles"' <<< "$gbs" \
      |tr -d '\n"[' |sed 's/: /:/' |sed 's/    / /g'
 echo ""
 echo ""
-bytesPrefix "\"disk space avail\": $diskAvail" "disk space avail"
+bytesPrefix "\"disk_avail\": $diskAvail" "disk_avail" "disk space avail" ""
 echo "  $systemLoad"
 echo ""
