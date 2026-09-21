be17e8ba30d735280a6f12c618de9ac33a231d5d
m28ray <307800295+m28ray@users.noreply.github.com>
2026-09-20 19:28:44 -0500

    Update btcnodemonit.sh
    Mention the license

--- btcnodemonit.sh
+++ btcnodemonit.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env bash
 
-# MIT License
-# 
-# Copyright (c) 2026 m28ray
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# Licensed under the MIT License
 
 ################################################
 #
@@ -37,7 +17,7 @@
 # User defined variables
 #
 # bitcoinDir
-# The filesystem (Example: /dev/sdb) or mount point (Example: /mnt/data)
+# The filesystem (Example: /dev/sdb1) or mount point (Example: /mnt/data)
 # of the bitcoin directory. If left blank the default is /.
 # Used by 'df' command to show free disk space.
 bitcoinDir="/"
